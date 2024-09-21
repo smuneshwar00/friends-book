d4625253f363e86bb4a9b3c10d0d5e763bcb4c7b
Swapnil Muneshwar <smuneshwar00@gmail.com>
2024-09-21 23:29:39 +0530

    Updated registration form with reactive form and updated data json

--- src/app/components/forms/registration-page/registration-page.component.ts
+++ src/app/components/forms/registration-page/registration-page.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'fb-registration-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './registration-page.component.html',
   styleUrl: './registration-page.component.css'
 })
-export class RegistrationPageComponent {
+export class RegistrationPageComponent implements OnInit{
+
+  registrationForm!: FormGroup;// Using the definite assignment assertion operator
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      dob: ['2000-01-01', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registrationForm.valid) {
+      console.log('Form Submitted');
+      console.log(this.registrationForm.value);
+    } else {
+      console.log('Form is invalid');
+      console.log(this.registrationForm);
+      console.log(this.registrationForm.errors?.['required']);
+    }
+  }
 
 }
